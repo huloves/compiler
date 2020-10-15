d4937d74b128b11b7a78e34e7f90d7d05be1ef9c
huloves <17791627852@163.com>
2020-10-15 20:19:27 +0800

    error begin

---
+++ lex/error.h
@@ -0,0 +1,16 @@
+#ifndef __LEX_ERROR_H
+#define __LEX_ERROR_H
+
+enum e_ErrorLevel
+{
+    LEVEL_WARNING,
+    LEVEL_ERROR,
+};
+
+enum e_WorkStage
+{
+    STAGE_COMPILE,
+    STAGE_LINK,
+};
+
+#endif
